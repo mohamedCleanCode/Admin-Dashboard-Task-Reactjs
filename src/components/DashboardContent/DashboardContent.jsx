27de99d699e25e5eee8f0ff9479814384763bf5e
mohamed <m635379@gmail.com>
2023-01-08 23:57:01 +0200

    add card-fetures styles

--- src/components/DashboardContent/DashboardContent.jsx
+++ src/components/DashboardContent/DashboardContent.jsx
@@ -1,4 +1,8 @@
-import { faArrowsUpDown, faBolt, faEgg } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowsUpDown,
+  faBolt,
+  faEgg,
+} from "@fortawesome/free-solid-svg-icons";
 import Navbar from "../Navbar";
 import CardFeatures from "./components/CardFeatures";
 import "./DashboardContent.css";
@@ -6,21 +10,21 @@ import "./DashboardContent.css";
 export default function DashboardContent() {
   return (
     <div className="DashboardContent ps-1">
-        <Navbar/>
-        <div className="row p-3">
-            <div className="col-3">
-                <CardFeatures icon={faBolt} title="Energy" ratio="45%"/>
-            </div>
-            <div className="col-3">
-                <CardFeatures icon={faArrowsUpDown} title="Range" ratio="157K%"/>
-            </div>
-            <div className="col-3">
-                <CardFeatures icon={faEgg} title="Break Fluid" ratio="9%"/>
-            </div>
-            <div className="col-3">
-                <CardFeatures icon={faEgg} title="Tire Wear" ratio="25%"/>
-            </div>
+      <Navbar />
+      <div className="row p-3">
+        <div className="col-3">
+          <CardFeatures icon={faBolt} title="Energy" ratio="45%" />
         </div>
+        <div className="col-3">
+          <CardFeatures icon={faArrowsUpDown} title="Range" ratio="157K%" />
+        </div>
+        <div className="col-3">
+          <CardFeatures icon={faEgg} title="Break Fluid" ratio="9%" />
+        </div>
+        <div className="col-3">
+          <CardFeatures icon={faEgg} title="Tire Wear" ratio="25%" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/DashboardContent/components/CardFeatures/CardFeatures.jsx
+++ src/components/DashboardContent/components/CardFeatures/CardFeatures.jsx
@@ -4,9 +4,38 @@ import "./CardFeatures.css";
 export default function CardFeatures(props) {
   return (
     <div className="CardFeatures pointer">
-      <FontAwesomeIcon icon={props.icon} className="icon"/>
+      <FontAwesomeIcon icon={props.icon} className="icon" />
       <div className="title">{props.title}</div>
-      <div className="ratio">{props.ratio}</div>
+      <div className="percent">
+        <div className="container">
+          <svg
+            className="circle-container"
+            viewBox="2 -2 28 36"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <linearGradient id="gradient">
+              <stop className="stop1" offset="0%" />
+              <stop className="stop2" offset="100%" />
+            </linearGradient>
+            <circle
+              className="circle-container__background"
+              r="16"
+              cx="16"
+              cy="16"
+              shapeRendering="geometricPrecision"
+            ></circle>
+            <circle
+              className="circle-container__progress"
+              r="16"
+              cx="16"
+              cy="16"
+              style={{ strokeDashoffset: "20" }}
+              shapeRendering="geometricPrecision"
+            ></circle>
+          </svg>
+        </div>
+        <div className="ratio">{props.ratio}</div>
+      </div>
     </div>
-  )
+  );
 }
